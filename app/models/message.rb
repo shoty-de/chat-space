9f11cbef4d63ee65f5e351d230dcc49ff5e3b0ea
shoty-de <66251668+shoty-de@users.noreply.github.com>
2020-07-01 19:51:42 +0900

    Update message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,8 +5,4 @@ class Message < ApplicationRecord
   validates :content, presence: true, unless: :image?
 
   mount_uploader :image, ImageUploader
-<<<<<<< HEAD
 end
-=======
-end
->>>>>>> 79fe9527969531dbf80bf0f6f1b470d88fb91a76
